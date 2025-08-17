cc7d4ec0ac7687519484fca507e8a606e663de13
Edi Soemartono <esjagad@gmail.com>
2025-08-18 00:15:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// import dependencies yang kita butuhkan di file ini
+// CommonJS --> const genai = require("@google/genai");
+// const { GoogleGenAI } = genai;
+import { GoogleGenAI } from "@google/genai"; // ESModule (ESM)
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+
+const { PORT } = process.env;
+
+// init Express-nya
+const app = express();
+
+// tambahkan middleware-nya ke dalam app
+app.use(cors()); // CORS: Cross-Origin Resource Sharing
+app.use(express.json()); // membolehkan request dengan Content-Type: application/json
+app.use(express.static('public'));
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({}); // instantiate sebuah class menjadi sebuah instance object di variable `ai`
+
+// tambahkan route handler
+// request untuk POST /chat
+// http://localhost:8086/chat --> POST
+// JSON body: { prompt: "" }
+
+app.post('/chat', async (req, res) => {
+
+  if (!req.body) {
+    
+    return res.status(400).send("Chat body tidak ada nih!");
+  }
+
+  const { messages } = req.body;
+  // messages: { role: 'user' | 'model', content: string }[]
+
+
+  if (!messages || !Array.isArray(messages)) {
+    return res.status(400).send(
+      "Pesan harus berupa array berisi role dan juga konten ! Tidak ada pesan atau pesannya nggak beraturan ! "
+    );
+  }
+
+  const contents = messages.map(message => {
+    return {
+      role: message.role,
+      parts: [
+        { text: message.content }
+      ]
+    }
+  })
+
+  try {
+    const aiResponse = await ai.models.generateContent({
+      config: {
+        systemInstruction: {
+          parts: [
+            { text: "terima kasih atas chat anda ! " }
+          ]
+        }
+      },
+      model: "gemini-2.5-flash-lite",
+      contents // shorthand dari "contents: contents"
+    })
+
+    return res.status(200).json({
+      response: aiResponse.text
+    });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+// membuat app-nya "mendengar" port [PORT]
+app.listen(PORT, () => {
+  console.log("Server Jadul Intel Prosesor : " + PORT);
+});
